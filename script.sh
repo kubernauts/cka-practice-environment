195b5dbfc325212238c5a93004405b60a82c6489
knrt10 <tripathi.kautilya@gmail.com>
2020-07-12 17:26:15 +0530

    Fix minikube profile
    It is not fixed that user will always run minikube cluster as default as
    specified in readme. They can do it like `minikube start -p
    test-cluster`. So to fix the problem, updated the script file.
    
    Signed-off-by: knrt10 <tripathi.kautilya@gmail.com>

--- script.sh
+++ script.sh
@@ -3,9 +3,11 @@
 USERNAME=`id -un`
 cd ~/.kube
 mkdir -p keys
+
+profile=$(minikube profile list -o json | jq '.valid' | jq -r '.[].Name')
 cp /Users/${USERNAME}/.minikube/ca.crt keys/
-cp /Users/${USERNAME}/.minikube/profiles/minikube/client.crt keys/
-cp /Users/${USERNAME}/.minikube/profiles/minikube/client.key keys/
+cp /Users/${USERNAME}/.minikube/profiles/$profile/client.crt keys/
+cp /Users/${USERNAME}/.minikube/profiles/$profile/client.key keys/
 
 gsed -i "s/\/Users\/${USERNAME}\/.minikube\/profiles\/minikube/.\/keys/g" config
 gsed -i "s/\/Users\/${USERNAME}\/.minikube/.\/keys/g" config
